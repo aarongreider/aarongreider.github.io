ebf8fa9dd58101539e8c06307ec1507496bf046a
Aaron Greider <aaron.greider@gmail.com>
2018-06-11 18:54:46 -0400

    menu click event

--- index_script.js
+++ index_script.js
@@ -1,1 +1,7 @@
 console.log('hello world');
+
+const menu = document.querySelector('.colorBar');
+
+menu.addEventListener('click', function (event) {
+    menu.classList.toggle('.activeColorBar')
+})
