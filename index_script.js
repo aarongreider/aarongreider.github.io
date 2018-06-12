218a6788ccbd1f037e197028f540d782031a48dd
Aaron Greider <aaron.greider@gmail.com>
2018-06-11 21:07:27 -0400

    correct Dropdown

--- index_script.js
+++ index_script.js
@@ -1,7 +1,39 @@
 console.log('hello wohald');
 
-const menu = document.querySelector('.colorBar');
+var aboutExpanded = false;
+var menuExpanded = false;
+var about;
+var menu;
 
-function myFunction() {
-    document.getElementById("colorBar").style.height = "100vh";
+
+function openAbout() {
+    about = document.getElementById("colorBar");
+    if (aboutExpanded) {
+        about.style.height = "3vh";
+        aboutExpanded = false;
+    } else {
+        about.style.height = "100vh";
+        aboutExpanded = true;
+
+        if (menuExpanded) {
+            openMenu();
+        }
+    }
+}
+
+function openMenu() {
+    menu = document.getElementById("colorBar2");
+    if (menuExpanded) {
+        menu.style.height = "3vh";
+        menu.style.top = "97vh";
+        menuExpanded = false;
+    } else {
+        menu.style.height = "100vh";
+        menu.style.top = "0";
+        menuExpanded = true;
+
+        if (aboutExpanded) {
+            openAbout();
+        }
+    }
 }
