654fe794a69378df7359e68a7b72ea393e5cb7f7
Aaron Greider <aaron.greider@gmail.com>
2018-07-17 12:03:55 -0400

    functional project nav buttons

--- index_script.js
+++ index_script.js
@@ -1,4 +1,4 @@
-console.log('hello whurled');
+console.log('hello werlad');
 
 var aboutExpanded = false;
 var menuExpanded = false;
@@ -44,6 +44,14 @@ function toggleMenu() {
     }
 }
 
+function checkToggled() {
+    if (menuExpanded) {
+        toggleMenu();
+    } else if (aboutExpanded) {
+        toggleAbout();
+    }
+}
+
 function expandProject() {
     gallery = document.getElementById("gallery");
     figure = document.getElementById("figure1");
@@ -53,22 +61,50 @@ function expandProject() {
         gallery.style.left = "0";
         nav.style.left = "0";
         galleryExpanded = false;
-        figure.style.filter = "grayscale(100%)"
+        figure.style.filter = "grayscale(100%)";
     } else {
         gallery.style.left = "-40vw";
         nav.style.left = "-40.8vw";
         galleryExpanded = true;
-        figure.style.filter = "grayscale(0%)"
+        figure.style.filter = "grayscale(0%)";
     }
 
-    if (menuExpanded) {
-        toggleMenu();
-    } else if (aboutExpanded) {
-        toggleAbout();
-    }
+    checkToggled();
 }
 
+function selectProject(circleNum) {
+    gallery = document.getElementById("gallery");
+    
+    switch(circleNum) {
+        case 1: 
+            gallery.style.top = "0";
+            break;
+        case 2:
+            gallery.style.top = "-68vh";
+            break;
+        case 3:
+            gallery.style.top = "-136vh";
+            break;
+        case 4:
+            gallery.style.top = "-204vh";
+            break;
+        case 5:
+            gallery.style.top = "-272vh";
+            break;
+        case 6:
+            gallery.style.top = "-340vh";
+            break;
+        case 7:
+            gallery.style.top = "-408vh";
+            break;
+        case 8:
+            gallery.style.top = "-476vh";
+            break;
+    }
 
+    projectNum = circleNum;
+    checkToggled();
+}
 
 window.addEventListener('wheel', function (e) {
     gallery = document.getElementById("gallery");
@@ -79,10 +115,10 @@ window.addEventListener('wheel', function (e) {
 
     if (e.deltaY > 0 && !galleryExpanded) {
         gallery.style.top = "-68vh";
-        projectNum = 1;
+        projectNum = 2;
     }
     else if (e.deltaY < 0  && !galleryExpanded) {
         gallery.style.top = "0";
-        projectNum = 0;
+        projectNum = 1;
     }
 });
