c250e11c865373f36172bd7f6320043944950e08
Aaron Greider <aaron.greider@gmail.com>
2022-08-06 20:05:08 -0400

    img srcs and content revisions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+(function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+            new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-TTM5PJ9');
+
+
 iconAsides();
 
 window.onload = function () {
@@ -42,7 +52,7 @@ function iconAsides() {
 function imgFlex() {
     // get all all imgs inside all flex-two tags
     // for each img, get aspect ratio // set its flex = img aspect ratio
-    let imgs = document.querySelectorAll(".flex-two > img");
+    let imgs = document.querySelectorAll(".flex-two > img, iframe");
     for (let i = 0; i < imgs.length; i++) {
         let ogHTML = imgs[i].outerHTML;
         imgs[i].outerHTML = `<div class="jsAdjust" style="flex: ${imgs[i].naturalWidth / imgs[i].naturalHeight}">${ogHTML}</div>`;
